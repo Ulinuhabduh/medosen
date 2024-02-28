de72c5ebc7e2a19e5d11dfd5596a964facc20d44
ulinnuhaaa <ulinnuhaabduh@gmail.com>
2024-02-28 20:44:45 +0700

    hampir selesai

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const dataHipnotik = {
   Propofol: {
     kegunaan: {
       Induksi: {
+        rute: "-",
         dosis: "2 - 2.5 mg/kg",
         minDosis: 2,
         maxDosis: 2.5,
@@ -11,6 +12,7 @@ const dataHipnotik = {
         DOA: "5 - 10 mnt",
       },
       Maintenance: {
+        rute: "-",
         dosis: "6 - 10 mg/kg/jam",
         minDosis: 6,
         maxDosis: 10,
@@ -18,6 +20,7 @@ const dataHipnotik = {
         DOA: "5 - 10 mnt",
       },
       Sedasi: {
+        rute: "-",
         dosis: "25 - 100 μg/kg/mnt",
         minDosis: 25,
         maxDosis: 100,
@@ -29,6 +32,7 @@ const dataHipnotik = {
   Thiopental: {
     kegunaan: {
       Induksi: {
+        rute: "-",
         dosis: "4 - 6 mg/kg",
         minDosis: 4,
         maxDosis: 6,
@@ -36,6 +40,7 @@ const dataHipnotik = {
         DOA: "5 - 15 mnt",
       },
       Maintenance: {
+        rute: "-",
         dosis: "1 - 3 mg/kg/jam",
         minDosis: 1,
         maxDosis: 3,
@@ -43,6 +48,7 @@ const dataHipnotik = {
         DOA: "5 - 15 mnt",
       },
       Sedasi: {
+        rute: "-",
         dosis: "1 - 3 mg/kg/jam",
         minDosis: 1,
         maxDosis: 3,
@@ -54,6 +60,7 @@ const dataHipnotik = {
   Ketamin: {
     kegunaan: {
       "Induksi IV": {
+        rute: "-",
         dosis: "1 - 3 mg/kg",
         minDosis: 1,
         maxDosis: 3,
@@ -61,6 +68,7 @@ const dataHipnotik = {
         DOA: "10 - 20 mnt",
       },
       "Intra Muskular": {
+        rute: "-",
         dosis: "8 - 13 mg/kg",
         minDosis: 8,
         maxDosis: 13,
@@ -74,92 +82,85 @@ const dataHipnotik = {
 const dataAnalgesik = {
   Morfin: {
     kegunaan: {
-      Premedikasi: {
-        rute: "Intra Muskuler",
+      "Premedikasi (Intra Muskuler)": {
         dosis: "0.05 - 0.2 mg/kg",
         minDosis: 0.05,
         maxDosis: 0.2,
         OOA: "5-10 mnt",
         DOA: "3-5 jam",
       },
-      "Intra Operasi": {
-        rute: "Intra Vena",
+      "Intra Operasi (Intra Vena)": {
+        rute: "",
         dosis: "0.1 - 1 mg/kg",
         minDosis: 0.1,
         maxDosis: 1,
         OOA: "5-10 mnt",
         DOA: "3-5 jam",
       },
-      "Post Operasi": {
-        rute: {
-          "Intra Muskuler": {
-            dosis: "0.05 - 0.2 mg/kg",
-            minDosis: 0.05,
-            maxDosis: 0.2,
-            OOA: "5-10 mnt",
-            DOA: "3-5 jam",
-          },
-          "Intra Vena": {
-            dosis: "0.03 - 0.15 mg/kg",
-            minDosis: 0.03,
-            maxDosis: 0.15,
-            OOA: "5-10 mnt",
-            DOA: "3-5 jam",
-          },
-        },
+      "Post Operasi (Intra Muskuler)": {
+        dosis: "0.05 - 0.2 mg/kg",
+        minDosis: 0.05,
+        maxDosis: 0.2,
+        OOA: "5-10 mnt",
+        DOA: "3-5 jam",
+      },
+      "Post Operasi (Intra Vena)": {
+        dosis: "0.03 - 0.15 mg/kg",
+        minDosis: 0.03,
+        maxDosis: 0.15,
+        OOA: "5-10 mnt",
+        DOA: "3-5 jam",
       },
     },
   },
   Petidin: {
     kegunaan: {
-      Premedikasi: {
-        rute: "Intra Muskuler",
+      "Premedikasi (Intra Muskuler)": {
+        rute: "",
         dosis: "0.5 - 1 mg/kg",
         minDosis: 0.5,
         maxDosis: 1,
         OOA: "5-10 mnt",
         DOA: "2-3 jam",
       },
-      "Intra Operatif": {
-        rute: "Intra Vena",
+      "Intra Operatif (Intra Vena)": {
+        rute: "",
         dosis: "2.5 - 5 mg/kg",
         minDosis: 2.5,
         maxDosis: 5,
         OOA: "5-10 mnt",
         DOA: "2-3 jam",
       },
-      "Post Operatif": {
-        rute: {
-          "Intra Muskuler": {
-            dosis: "0.5 - 1 mg/kg",
-            minDosis: 0.5,
-            maxDosis: 1,
-            OOA: "5-10 mnt",
-            DOA: "2-3 jam",
-          },
-          "Intra Vena": {
-            dosis: "0.2 - 0.5 mg/kg",
-            minDosis: 0.2,
-            maxDosis: 0.5,
-            OOA: "5-10 mnt",
-            DOA: "2-3 jam",
-          },
-        },
+      "Post Operatif (Intra Muskuler)": {
+        rute: "",
+        dosis: "0.5 - 1 mg/kg",
+        minDosis: 0.5,
+        maxDosis: 1,
+        OOA: "5-10 mnt",
+        DOA: "2-3 jam",
+      },
+      "Post Operatif (Intra Vena)": {
+        rute: "",
+        dosis: "0.2 - 0.5 mg/kg",
+        minDosis: 0.2,
+        maxDosis: 0.5,
+        OOA: "5-10 mnt",
+        DOA: "2-3 jam",
       },
     },
   },
   Fentanyl: {
     kegunaan: {
-      "Intra Operatif": {
-        rute: "Intra Vena",
+      "Intra Operatif (Intra Vena)": {
+        rute: "",
         dosis: "2 - 50 mcg/kg",
         minDosis: 2,
         maxDosis: 50,
         OOA: "2 mnt",
         DOA: "45 mnt - 2 jam",
       },
-      "Post Operatif": {
-        rute: "Intra Vena",
+      "Post Operatif (Intra Vena)": {
+        rute: "",
         dosis: "0.5 - 1.5 mcg/kg",
         minDosis: 0.5,
         maxDosis: 1.5,
@@ -170,16 +171,16 @@ const dataAnalgesik = {
   },
   Sufetanil: {
     kegunaan: {
-      Analgesia: {
-        rute: "Intra Muskuler / Intra Vena",
+      "Analgesia (Intra Muskuler / Intra Vena)": {
+        rute: "",
         dosis: "0.2 - 0.6 mcg/kg",
         minDosis: 0.2,
         maxDosis: 0.6,
         OOA: "1 - 3 mnt",
         DOA: "IV: 20 - 45 mnt IM: 2 - 4 Jam",
       },
-      Induksi: {
-        rute: "Intra Vena",
+      "Induksi (Intra Vena)": {
+        rute: "",
         dosis: "2 - 10 mcg/kg",
         minDosis: 2,
         maxDosis: 10,
@@ -190,16 +191,16 @@ const dataAnalgesik = {
   },
   Alfetanil: {
     kegunaan: {
-      Analgesia: {
-        rute: "Intra Muskuler / Intra Vena",
+      "Analgesia (Intra Muskuler / Intra Vena)": {
+        rute: "",
         dosis: "5 - 10 mcg/kg",
         minDosis: 5,
         maxDosis: 10,
         OOA: "1 - 2 mnt",
         DOA: "1 - 2 Jam",
       },
-      Induksi: {
-        rute: "Intra Vena",
+      "Induksi (Intra Vena)": {
+        rute: "",
         dosis: "50 - 300 mcg/kg",
         minDosis: 50,
         maxDosis: 300,
@@ -210,16 +211,16 @@ const dataAnalgesik = {
   },
   Remifeentanil: {
     kegunaan: {
-      Analgesia: {
-        rute: "Intra Vena",
+      "Analgesia (Intra Vena)": {
+        rute: "",
         dosis: "0.5 - 1.0 mg/kg",
         minDosis: 0.5,
         maxDosis: 2.5,
         OOA: "30s",
         DOA: "5 - 10 mnt",
       },
-      Induksi: {
-        rute: "Intra Vena",
+      "Induksi (Intra Vena)": {
+        rute: "",
         dosis: "1 mcg/kg",
         fixDosis: 1,
         OOA: "30s",
@@ -229,15 +230,15 @@ const dataAnalgesik = {
   },
   Buprenorphine: {
     kegunaan: {
-      Premedikasi: {
-        rute: "Intra Vena",
+      "Premedikasi (Intra Vena)": {
+        rute: "",
         dosis: "400 mcg/kg",
         fixDosis: 400,
         OOA: "30 mnt",
         DOA: "24 jam",
       },
-      Analgesia: {
-        rute: "Intra Vena",
+      "Analgesia (Intra Vena)": {
+        rute: "",
         dosis: "200 - 400 mcg/kg",
         minDosis: 200,
         maxDosis: 400,
@@ -248,26 +249,22 @@ const dataAnalgesik = {
   },
   Hydromorphone: {
     kegunaan: {
-      Analgesia: {
-        rute: {
-          "Intra Vena": {
-            dosis: "0.01 - 0.04 mg/kg",
-            minDosis: 0.01,
-            maxDosis: 0.04,
-            OOA: "15 - 30 mnt",
-            DOA: "4 - 6 jam",
-          },
-          "Intra Muskuler": {
-            dosis: "0.04 - 0.08 mg/kg",
-            minDosis: 0.04,
-            maxDosis: 0.08,
-            OOA: "15 - 30 mnt",
-            DOA: "4 - 6 jam",
-          },
-        },
+      "Analgesia (Intra Vena)": {
+        dosis: "0.01 - 0.04 mg/kg",
+        minDosis: 0.01,
+        maxDosis: 0.04,
+        OOA: "15 - 30 mnt",
+        DOA: "4 - 6 jam",
       },
-      "Post Operatif": {
-        rute: "Intra Vena",
+      "Anagesia (Intra Muskuler)": {
+        dosis: "0.04 - 0.08 mg/kg",
+        minDosis: 0.04,
+        maxDosis: 0.08,
+        OOA: "15 - 30 mnt",
+        DOA: "4 - 6 jam",
+      },
+      "Post Operatif (Intra Vena)": {
+        rute: "",
         dosis: "2 - 10 mcg/kg",
         minDosis: 2,
         maxDosis: 10,
@@ -275,45 +272,43 @@ const dataAnalgesik = {
         DOA: "4 - 6 jam",
       },
     },
-  },
-  Diamorphine: {
-    kegunaan: {
-      Analgesia: {
-        rute: "Intra Vena",
-        dosis: "< 400 mg/injeksi",
-        fixDosis: 400,
-        OOA: "2 - 3 mnt",
-        DOA: "-",
+    Diamorphine: {
+      kegunaan: {
+        "Analgesia (Intra Vena)": {
+          rute: "",
+          dosis: "< 400 mg/injeksi",
+          fixDosis: 400,
+          OOA: "2 - 3 mnt",
+          DOA: "-",
+        },
+      },
+    },
+    Tramadol: {
+      kegunaan: {
+        "Post Operatif (Intra Vena)": {
+          rute: "",
+          dosis: "50 - 100 mg/kg",
+          minDosis: 50,
+          maxDosis: 100,
+          OOA: "1 jam",
+          DOA: "6 - 9 jam",
+        },
       },
     },
   },
-  Tramadol: {
-    kegunaan: {
-      "Post Operatif": {
-        rute: "Intra Vena",
-        dosis: "50 - 100 mg/kg",
-        minDosis: 50,
-        maxDosis: 100,
-        OOA: "1 jam",
-        DOA: "6 - 9 jam",
-      },
-    },
-  },
-};
+}
 
 const dataResusitasi = {
   Ephedrine: {
     kegunaan: {
-      "Hipotensi/Bronkospasme": {
-        rute: {
-          "Intra Vena": {
+      "Hipotensi/Bronkospasme (Intra Vena)": {
             dosis: "5 - 20 mg",
             minDosis: 5,
             maxDosis: 20,
             OOA: "2 - 5 mnt",
             DOA: "10 - 60 mnt",
           },
-          "Intra Muskuler": {
+      "Hipotensi/Bronkospasme (Intra Muskuler)": {
             dosis: "25 - 50 mg",
             minDosis: 25,
             maxDosis: 50,
@@ -322,8 +317,6 @@ const dataResusitasi = {
           },
         },
       },
-    },
-  },
   "Sulfat Atropin": {
     kegunaan: {
       "Bradikardia Sinus": {
@@ -348,67 +341,6 @@ const dataResusitasi = {
     },
   },
 };
-
-
-// ----------------------------------------------------------------------------------------------------
-
-
-document.getElementById("calculationForm").addEventListener("submit", function (event) {
-  event.preventDefault(); // Prevent the form from submitting and refreshing the page
-
-  // Get user inputs
-  const selectedObat = document.getElementById("obat").value;
-  const spuit = parseInt(document.getElementById("spuit").value);
-  const sediaan = parseFloat(document.getElementById("sediaan").value);
-  const beratBadan = parseFloat(document.getElementById("berat_badan").value);
-
-  // Get the selected data based on the category of the selectedObat
-  let selectedData;
-  if (dataHipnotik[selectedObat]) {
-    selectedData = dataHipnotik[selectedObat].kegunaan;
-  } else if (dataAnalgesik[selectedObat]) {
-    selectedData = dataAnalgesik[selectedObat].kegunaan;
-  } else if (dataResusitasi[selectedObat]) {
-    selectedData = dataResusitasi[selectedObat].kegunaan;
-  }
-
-  // Generate HTML for displaying dosage information
-  let dosageInfoHTML = "<h3>Informasi Dosis Pengenceran</h3>";
-  if (selectedData) {
-    for (const kegunaan in selectedData) {
-      if (selectedData.hasOwnProperty(kegunaan)) {
-        const rute = selectedData[kegunaan].rute || "-";
-        const dosisPengenceranMin = selectedData[kegunaan].minDosis ? ((selectedData[kegunaan].minDosis * beratBadan) / (sediaan * 10000)) * spuit : "-";
-        const dosisPengenceranMax = selectedData[kegunaan].maxDosis ? ((selectedData[kegunaan].maxDosis * beratBadan) / (sediaan * 10000)) * spuit : "-";
-        const dosisPengenceran = selectedData[kegunaan].fixDosis ? ((selectedData[kegunaan].fixDosis * beratBadan) / (sediaan * 10000)) * spuit : "-";
-
-        // Generate dosage information based on conditions
-        let dosageInfo;
-        if (dosisPengenceran !== "-") {
-          dosageInfo = `${kegunaan} => ${rute} : ${dosisPengenceran.toFixed(2)} ml`;
-        } else if (dosisPengenceranMin !== "-" && dosisPengenceranMax !== "-") {
-          dosageInfo = `${kegunaan} => ${rute} : ${dosisPengenceranMin.toFixed(2)} ml ---> ${dosisPengenceranMax.toFixed(2)} ml`;
-        } else if (selectedData[kegunaan].minDosisDewasa && selectedData[kegunaan].maxDosisDewasa && selectedData[kegunaan].minDosisAnak && selectedData[kegunaan].maxDosisAnak) {
-          const dosisPengenceranMinDewasa = ((selectedData[kegunaan].minDosisDewasa * beratBadan) / (sediaan * 10000)) * spuit;
-          const dosisPengenceranMaxDewasa = ((selectedData[kegunaan].maxDosisDewasa * beratBadan) / (sediaan * 10000)) * spuit;
-          const dosisPengenceranMinAnak = ((selectedData[kegunaan].minDosisAnak * beratBadan) / (sediaan * 10000)) * spuit;
-          const dosisPengenceranMaxAnak = ((selectedData[kegunaan].maxDosisAnak * beratBadan) / (sediaan * 10000)) * spuit;
-          dosageInfo = `${kegunaan} => Dewasa : ${dosisPengenceranMinDewasa.toFixed(2)} ml ---> ${dosisPengenceranMaxDewasa.toFixed(2)} ml, Anak : ${dosisPengenceranMinAnak.toFixed(2)} ml ---> ${dosisPengenceranMaxAnak.toFixed(2)} ml`;
-        } else {
-          dosageInfo = `${kegunaan} => ${dosisPengenceranMin.toFixed(2)} ml ---> ${dosisPengenceranMax.toFixed(2)} ml`;
-        }
-
-        dosageInfoHTML += `<p>${dosageInfo}</p>`;
-      }
-    }
-  } else {
-    dosageInfoHTML += `<p>Data obat tidak ditemukan.</p>`;
-  }
-
-  // Display dosage information
-  const hasilElement = document.querySelector(".hasil");
-  hasilElement.innerHTML = dosageInfoHTML;
-});
 
 // ----------------------------------------------------------------------------------------------------------------------
 
@@ -437,7 +369,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Menampilkan informasi dosis pengenceran
     const infoDosis = document.createElement("div");
-    infoDosis.innerHTML = `<h3>Dosis Pengenceran ${obat}</h3>`;
+    infoDosis.innerHTML = `<p class="obat text-center text-primary">Dosis Pengenceran ${obat}</p>`;
+    const infoBerat = `<p class="fs-5 text-center text-dark">Berat Badan : ${beratBadan} kg</p>`;
+    infoDosis.innerHTML += infoBerat;
+    const infoSediaan = `<p class="fs-5 text-center text-dark">Sediaan : ${sediaan} gr/ml</p>`;
+    infoDosis.innerHTML += infoSediaan;
 
     if (dataObat) {
       for (const kegunaan in dataObat.kegunaan) {
@@ -447,19 +383,30 @@ document.addEventListener("DOMContentLoaded", function () {
         let dosis;
         if (kegunaanData.fixDosis !== undefined) {
           dosis = ((kegunaanData.fixDosis * beratBadan) / (sediaan * 10000)) * spuit;
-        } else {
+        } else if (kegunaanData.minDosis !== undefined || kegunaanData.maxDosis !== undefined) {
           dosis = {
             min: ((kegunaanData.minDosis * beratBadan) / (sediaan * 10000)) * spuit,
             max: ((kegunaanData.maxDosis * beratBadan) / (sediaan * 10000)) * spuit,
           };
+        } else {
+          dosis = {
+            minDewasa: ((kegunaanData.minDosisDewasa) / (sediaan * 10000)) * spuit,
+            maxDewasa: ((kegunaanData.maxDosisDewasa) / (sediaan * 10000)) * spuit,
+
+            minAnak: ((kegunaanData.minDosisAnak * beratBadan) / (sediaan * 10000)) * spuit,
+            maxAnak: ((kegunaanData.maxDosisAnak * beratBadan) / (sediaan * 10000)) * spuit,
+          }
         }
 
         // Menampilkan informasi dosis pengenceran sesuai format
         let dosisInfo;
         if (kegunaanData.fixDosis !== undefined) {
-          dosisInfo = `<p>${kegunaan} => Dosis Pengenceran: ${dosis.toFixed(5)} ml</p>`;
+          dosisInfo = `<p><strong class="text-success">${kegunaan}</strong> => <strong></strong> Dosis Pengenceran: <strong>${dosis.toFixed(5)}</strong> ml</p>`;
+        } else if (kegunaanData.minDosis !== undefined || kegunaanData.maxDosis !== undefined) {
+          dosisInfo = `<p><strong class="text-success">${kegunaan}</strong> => <strong></strong> Dosis Pengenceran: <strong>${dosis.min.toFixed(5)}</strong> - <strong>${dosis.max.toFixed(5)}</strong> ml</p>`;
         } else {
-          dosisInfo = `<p>${kegunaan} => Dosis Pengenceran: ${dosis.min.toFixed(5)} - ${dosis.max.toFixed(5)} ml</p>`;
+          dosisInfo = `<p><strong class="text-success">${kegunaan}</strong> => Dosis Pengenceran Dewasa: <strong>${dosis.minDewasa.toFixed(5)}</strong> - <strong>${dosis.maxDewasa.toFixed(5)}</strong> ml</p>`;
+          dosisInfo += `<p><strong class="text-success">${kegunaan}</strong> => Dosis Pengenceran Anak: <strong>${dosis.minAnak.toFixed(5)}</strong> - <strong>${dosis.maxAnak.toFixed(5)}</strong> ml</p>`;
         }
         infoDosis.innerHTML += dosisInfo;
       }
@@ -475,7 +422,7 @@ document.addEventListener("DOMContentLoaded", function () {
       existingInfo.replaceWith(infoDosis);
     } else {
       infoDosis.classList.add("result");
-      form.appendChild(infoDosis);
+      form.insertBefore(infoDosis, form.childNodes[10]);
     }
   });
 });
